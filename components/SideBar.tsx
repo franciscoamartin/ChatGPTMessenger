e1254a3bfc27dac25d2abfa3f2d323f65581f8da
Francisco Augusto Martin <francisco.martin@ambevtech.com.br>
2023-02-24 15:25:51 -0300

    add google auth

---
+++ components/SideBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NewChat from './NewChat'
+
+function SideBar() {
+  return (
+    <div className='p-2 flex flex-col h-screen'>
+      <div className='flex-1'>
+        <div>
+          <NewChat />
+          <div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
