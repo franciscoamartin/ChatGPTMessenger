032a00b2880ba2ef38a658f1aef33519dc5e70e4
Francisco Augusto Martin <francisco.martin@ambevtech.com.br>
2023-03-10 15:09:45 -0300

    connecting openai api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { getServerSession } from 'next-auth'
+import ClientProvider from '../components/ClientProvider'
 import Login from '../components/Login'
 import { SessionProvider } from '../components/SessionProvider'
 import SideBar from '../components/SideBar'
@@ -12,7 +13,7 @@ export default async function RootLayout({
 }) {
 
   const session = await getServerSession(authOptions)
-  
+
   return (
     <html>
       <head />
@@ -23,6 +24,7 @@ export default async function RootLayout({
               <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
                 <SideBar />
               </div>
+              <ClientProvider />
               <div className='bg-[#343541] flex-1'>
                 {children}
               </div>
